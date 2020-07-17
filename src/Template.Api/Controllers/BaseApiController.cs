9d9eda08bcefb1cc6aacbf41f90febfbc108f762
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-07-17 23:33:09 +0200

    add ActionCallContext and BaseApiController

---
+++ src/Template.Api/Controllers/BaseApiController.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Template.Domain.Models;
+
+namespace Template.Api.Controllers
+{
+    public class BaseApiController : ControllerBase
+    {
+        protected ActionCallContext _actionCallContext;
+
+        public BaseApiController(ClaimsPrincipal claimsPrincipal)
+        {
+            _actionCallContext = new ActionCallContext(claimsPrincipal);
+        }
+    }
+}
---
+++ src/Template.Domain/Models/ActionCallContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Security.Claims;
+
+namespace Template.Domain.Models
+{
+    public class ActionCallContext
+    {
+        public ActionCallContext(ClaimsPrincipal principal)
+        {
+            Principal = principal;
+            UserId = Convert.ToInt32(principal.FindFirst(c => c.Type == ClaimTypes.NameIdentifier)?.Value ?? "0");
+            Username = principal.FindFirst(c => c.Type == ClaimTypes.Name)?.Value;
+            Email = principal.FindFirst(c => c.Type == ClaimTypes.Email)?.Value;
+        }
+
+        public ClaimsPrincipal Principal { get; }
+
+        public int UserId { get; set; }
+
+        public string Email { get; }
+
+        public string Username { get; }
+    }
+}
